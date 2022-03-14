f014800e345c30ba57b2aea5c2f4f2742807174b
YanisMaybe <96627870+YanisMaybe@users.noreply.github.com>
2022-03-14 01:09:06 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,104 @@
+const App = () => {
+
+  const firstImage = "https://www.overtheseadress.com/wp-content/uploads/2020/12/love-dress-white-04-760x1140.jpg";
+  const secondImage = "https://www.overtheseadress.com/wp-content/uploads/2020/12/muse-dress-beige-floral-03-760x1140.jpg";
+
+  return(
+    <div className = "myWebsite">
+      <div className="conainerOfMyAll">
+        <header className="header">
+          <div className="containerOfHeader">
+            <div className="overtheseaText">
+              <h3>OVERTHESEA</h3>
+            </div>
+            <div className="menuButton btn">
+              <div className="bracket"></div>
+              <div className="bracket"></div>
+            </div>
+          </div>
+        </header>
+        <div className="middlePart">
+          <div className="containerOfMiddlePart">
+            <div className="firstBlocMDL">
+              <div className="theEuphoriaText">
+                <span>
+                  <h1 className="VoyageFont">30 fellings</h1>
+                </span>
+              </div>
+            </div>
+            <div className="secondBlocMDL">
+              <div className="containerOfSecondBloc">
+
+              </div>
+            </div>
+            <div className="therdBlocMDL">
+              <div className="containerOfTherdBloc">
+                <div className="imageAbsParent">
+                  <div className="imageAbsolutly">
+                    
+                    </div>
+                </div>
+                <div className="buttonLink">
+                  <div className="contOfbl">
+                      <div className="preorferSe">
+                        <p className="btn">preorder now</p>
+                        <div className="arrow"></div>
+                      </div>
+                      <div className="svgImage">
+                        <svg fill="#d58544" version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                          viewBox="0 0 575.43 242.12" xmlSpace="preserve">
+                            
+                        <g>
+                          <g>
+                            <path d="M339.07,11.17c43.47,16.12,85.76,35.35,126.47,57.53c19.99,10.89,40.07,22.15,58.91,34.95
+                              c9.01,6.13,17.62,13.36,22.92,23.06c4.8,8.79,6.39,19.17,2.5,28.61c-7.13,17.28-27.02,26.05-43.06,32.89
+                              c-21.3,9.08-43.44,16.18-65.89,21.8c-45.79,11.46-93.05,16.99-140.13,19.51c-48.82,2.62-97.84,1.41-146.47-3.61
+                              c-21.98-2.27-44.35-4.61-65.69-10.59c-18.93-5.31-37.81-14.21-50.42-29.78c-10.78-13.32-17.5-31.74-14.03-48.92
+                              c3.81-18.89,19.49-30.6,36.35-37.7c9.39-3.95,19.22-6.81,29-9.62c10.94-3.14,21.97-6,33.06-8.58
+                              c22.27-5.18,44.81-9.24,67.49-12.12c45.19-5.74,90.94-6.97,136.37-3.71c45.53,3.27,90.71,11.11,134.66,23.44
+                              c10.65,2.99,21.23,6.25,31.73,9.77c3.06,1.02,4.37-3.8,1.33-4.82c-44.64-14.94-90.79-25.12-137.54-30.55
+                              c-46.84-5.45-94.23-6.07-141.2-1.87c-46.53,4.16-92.69,12.99-137.38,26.63c-18.14,5.54-37.62,12.68-49.64,28.2
+                              c-11.12,14.36-12.41,33.31-6.68,50.15c6.2,18.21,19.22,32.65,35.76,42.17c18.55,10.68,39.85,15.35,60.81,18.48
+                              c49.19,7.32,99.22,10.13,148.93,9.25c49-0.87,98.29-5.13,146.42-14.54c23.44-4.58,46.63-10.55,69.19-18.41
+                              c11.09-3.86,22.02-8.18,32.74-12.98c8.69-3.89,17.12-8.4,24.55-14.4c6.86-5.54,12.72-12.62,15.41-21.12
+                              c3.11-9.82,1.26-20.31-3.44-29.3c-5.08-9.74-13.33-17.25-22.16-23.56c-9.34-6.69-19.35-12.52-29.21-18.4
+                              c-40.89-24.37-83.58-45.7-127.59-63.84c-10.84-4.47-21.76-8.73-32.75-12.81C337.38,5.23,336.07,10.06,339.07,11.17L339.07,11.17z"
+                              />
+                          </g>
+                        </g>
+                        </svg>
+                      </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer className="footer">
+          <div className="containerOfFooter">
+            <div className="firstPartOfFooter">
+              <div className="containerOfLast">
+                <p className="btn">Instagram</p>
+                <p className="btn">Facebook</p>
+                <p className="btn">YouTube</p>
+                <p className="btn">Pinterest</p>
+              </div>
+            </div>
+            <div className="secondPartOfFooter">
+              <div className="containerOfsecondPartOfFooter">
+                <div className="meldoum">
+
+                </div>
+                <div className="credits">
+                  <p> 2016 - 2021 Overthesea</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default App;
